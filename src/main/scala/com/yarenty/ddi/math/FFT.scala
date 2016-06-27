64c38089d00c48ff883b53829d8cba8fb23710f8
yarenty <yarenty@gmail.com>
2016-06-27 13:14:02 +0100

    added some math stuff

---
+++ src/main/scala/com/yarenty/ddi/math/FFT.scala
@@ -0,0 +1,62 @@
+package com.yarenty.ddi.math
+
+import scala.math._
+
+/**
+  * Created by yarenty on 27/06/2016.
+  * (C)2015 SkyCorp Ltd.
+  */
+
+
+case class Complex(re: Double, im: Double = 0.0) {
+  def +(x: Complex): Complex = Complex((this.re + x.re), (this.im + x.im))
+
+  def -(x: Complex): Complex = Complex((this.re - x.re), (this.im - x.im))
+
+  def *(x: Complex): Complex = Complex(this.re * x.re - this.im * x.im, this.re * x.im + this.im * x.re)
+}
+
+class FFT {
+  def transformReal(input: IndexedSeq[Double]) = {
+    val data = padder(input.map(i => Complex(i)).toList)
+    val outComplex = fft(data)
+    outComplex.map(c => math.sqrt((c.re * c.re) + (c.im * c.im))).take((data.length / 2) + 1).toIndexedSeq // Magnitude Output
+  }
+
+  def powerSpectrum(input: IndexedSeq[Double]) = {
+    val data = padder(input.map(i => Complex(i)).toList)
+    val outComplex = fft(data)
+    val out = outComplex.map(c => math.sqrt((c.re * c.re) + (c.im * c.im))).take((data.length / 2) + 1).toIndexedSeq
+    out.map(i => (i * i) / data.length) // Power Spectral Density Output
+  }
+
+  def padder(data: List[Complex]): List[Complex] = {
+    def check(num: Int): Boolean = if ((num.&(num - 1)) == 0) true else false
+    def pad(i: Int): Int = {
+      check(i) match {
+        case true => i
+        case false => pad(i + 1)
+      }
+    }
+    if (check(data.length) == true) data else data.padTo(pad(data.length), Complex(0))
+  }
+
+  def fft(f: List[Complex]): List[Complex] = {
+    f.size match {
+      case 0 => Nil
+      case 1 => f
+      case n => {
+        val c: Double => Complex = phi => Complex(cos(phi), sin(phi))
+        val e = fft(f.zipWithIndex.filter(_._2 % 2 == 0).map(_._1))
+        val o = fft(f.zipWithIndex.filter(_._2 % 2 != 0).map(_._1))
+        def it(in: List[(Int, Complex)], k: Int = 0): List[(Int, Complex)] = {
+          k < (n / 2) match {
+            case true => it((k + n / 2, e(k) - o(k) * c(-2 * Pi * k / n)) ::(k, e(k) + o(k) * c(-2 * Pi * k / n)) :: in, k + 1)
+            case false => in
+          }
+        }
+        it(List[(Int, Complex)]()).sortWith((x, y) => x._1 < y._1).map(_._2)
+      }
+    }
+  }
+}
---
+++ src/main/scala/com/yarenty/ddi/math/Granger.scala
@@ -0,0 +1,106 @@
+package com.yarenty.ddi.math
+
+
+import org.apache.commons.math.MathException
+import org.apache.commons.math.distribution.FDistribution
+import org.apache.commons.math.distribution.FDistributionImpl
+import org.apache.commons.math.stat.StatUtils
+import org.apache.commons.math.stat.regression.OLSMultipleLinearRegression
+
+
+/**
+  * Based on Sergey Edunov java solution.
+  * Created by yarenty on 27/06/2016.
+  * (C)2015 SkyCorp Ltd.
+  */
+
+
+class GrangerTestResult(fStat: Double, r3: Double, pValue: Double) {
+}
+
+class Granger {
+
+  /**
+    * Returns p-value for Granger causality test.
+    *
+    * @param y - predictable variable
+    * @param x - predictor
+    * @param L - lag, should be 1 or greater.
+    * @return p-value of Granger causality
+    */
+  def granger(y: Array[Double], x: Array[Double], L: Int): GrangerTestResult = {
+    val h0 = new OLSMultipleLinearRegression()
+    val h1 = new OLSMultipleLinearRegression()
+
+    val laggedY = createLaggedSide(L, Array(y))
+
+    val laggedXY = createLaggedSide(L, Array(x, y))
+
+    val n = laggedY.length
+
+    h0.newSampleData(strip(L, y), laggedY)
+    h1.newSampleData(strip(L, y), laggedXY)
+
+    val rs0 = h0.estimateResiduals()
+    val rs1 = h1.estimateResiduals()
+
+    val TSS1 = tss(strip(L, y))
+
+    val RSS0 = sqrSum(rs0)
+    val RSS1 = sqrSum(rs1)
+
+    val ftest = ((RSS0 - RSS1) / L) / (RSS1 / (n - 2 * L - 1))
+
+    val fDist = new FDistributionImpl(L, n - 2 * L - 1)
+    try {
+      val pValue = 1.0 - fDist.cumulativeProbability(ftest)
+      return new GrangerTestResult(ftest, (1 - RSS1 / TSS1), pValue)
+    } catch {
+      case e: MathException => throw new RuntimeException(e)
+    }
+  }
+
+  def tss(y: Array[Double]): Double = {
+    var res = 0.0
+    val avg = StatUtils.mean(y)
+    for (yi <- y) {
+      res += (yi - avg) * (yi - avg);
+    }
+    res
+  }
+
+
+  def createLaggedSide(L: Int, a: Array[Array[Double]]): Array[Array[Double]] = {
+    val n = a(0).length - L
+    val res = Array.ofDim[Double](n, L * a.length + 1)
+
+    for (i <- 0 to a.length) {
+      val ai = a(i)
+      for (l <- 0 to L) {
+        for (j <- 0 to n) {
+          res(j)(i * L + l) = ai(l + j)
+        }
+      }
+    }
+
+    for (i <- 0 to n) {
+      res(i)(L * a.length) = 1
+    }
+    res
+  }
+
+  def sqrSum(a: Array[Double]): Double = {
+    var res = 0.0
+    for (v <- a) {
+      res += v * v
+    }
+    res
+  }
+
+  def strip(l: Int, a: Array[Double]): Array[Double] = {
+    val res = Array[Double](a.length - l)
+    System.arraycopy(a, l, res, 0, res.length);
+    res
+  }
+
+}
