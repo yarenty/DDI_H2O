5e2ce6dc05e490395398f1c24804a5600723cad9
yarenty <yarenty@gmail.com>
2016-06-29 12:22:46 +0100

    and POI data

---
+++ src/main/scala/com/yarenty/ddi/schemas/POI.scala
@@ -0,0 +1,323 @@
+package com.yarenty.ddi.schemas
+
+/**
+  * Created by yarenty on 29/06/2016.
+  * (C)2015 SkyCorp Ltd.
+  */
+class POI(val C1: Option[String],
+val C2: Option[String],
+val C3: Option[String],
+val C4: Option[String],
+val C5: Option[String],
+val C6: Option[String],
+val C7: Option[String],
+val C8: Option[String],
+val C9: Option[String],
+val C10: Option[String],
+val C11: Option[String],
+val C12: Option[String],
+val C13: Option[String],
+val C14: Option[String],
+val C15: Option[String],
+val C16: Option[String],
+val C17: Option[String],
+val C18: Option[String],
+val C19: Option[String],
+val C20: Option[String],
+val C21: Option[String],
+val C22: Option[String],
+val C23: Option[String],
+val C24: Option[String],
+val C25: Option[String],
+val C26: Option[String],
+val C27: Option[String],
+val C28: Option[String],
+val C29: Option[String],
+val C30: Option[String],
+val C31: Option[String],
+val C32: Option[String],
+val C33: Option[String],
+val C34: Option[String],
+val C35: Option[String],
+val C36: Option[String],
+val C37: Option[String],
+val C38: Option[String],
+val C39: Option[String],
+val C40: Option[String],
+val C41: Option[String],
+val C42: Option[String],
+val C43: Option[String],
+val C44: Option[String],
+val C45: Option[String],
+val C46: Option[String],
+val C47: Option[String],
+val C48: Option[String],
+val C49: Option[String],
+val C50: Option[String],
+val C51: Option[String],
+val C52: Option[String],
+val C53: Option[String],
+val C54: Option[String],
+val C55: Option[String],
+val C56: Option[String],
+val C57: Option[String],
+val C58: Option[String],
+val C59: Option[String],
+val C60: Option[String],
+val C61: Option[String],
+val C62: Option[String],
+val C63: Option[String],
+val C64: Option[String],
+val C65: Option[String],
+val C66: Option[String],
+val C67: Option[String],
+val C68: Option[String],
+val C69: Option[String],
+val C70: Option[String],
+val C71: Option[String],
+val C72: Option[String],
+val C73: Option[String],
+val C74: Option[String],
+val C75: Option[String],
+val C76: Option[String],
+val C77: Option[String],
+val C78: Option[String],
+val C79: Option[String],
+val C80: Option[String],
+val C81: Option[String],
+val C82: Option[String],
+val C83: Option[String],
+val C84: Option[String],
+val C85: Option[String],
+val C86: Option[String],
+val C87: Option[String],
+val C88: Option[String],
+val C89: Option[String],
+val C90: Option[String],
+val C91: Option[String],
+val C92: Option[String],
+val C93: Option[String],
+val C94: Option[String],
+val C95: Option[String],
+val C96: Option[String],
+val C97: Option[String],
+val C98: Option[String],
+val C99: Option[String],
+val C100: Option[String],
+val C101: Option[String],
+val C102: Option[String],
+val C103: Option[String],
+val C104: Option[String],
+val C105: Option[String],
+val C106: Option[String],
+val C107: Option[String],
+val C108: Option[String],
+val C109: Option[String],
+val C110: Option[String],
+val C111: Option[String],
+val C112: Option[String],
+val C113: Option[String],
+val C114: Option[String],
+val C115: Option[String],
+val C116: Option[String],
+val C117: Option[String],
+val C118: Option[String],
+val C119: Option[String],
+val C120: Option[String],
+val C121: Option[String],
+val C122: Option[String],
+val C123: Option[String],
+val C124: Option[String],
+val C125: Option[String],
+val C126: Option[String],
+val C127: Option[String],
+val C128: Option[String],
+val C129: Option[String],
+val C130: Option[String],
+val C131: Option[String],
+val C132: Option[String],
+val C133: Option[String],
+val C134: Option[String],
+val C135: Option[String],
+val C136: Option[String],
+val C137: Option[String],
+val C138: Option[String],
+val C139: Option[String]) extends Product with Serializable {
+
+  override def canEqual(that: Any): Boolean = that.isInstanceOf[Order]
+
+  override def productArity: Int = 139
+
+  override def productElement(n: Int) = n match {
+    case 0 => C1
+    case 1 => C2
+    case 2 => C3
+    case 3 => C4
+    case 4 => C5
+    case 5 => C6
+    case 6 => C7
+    case 7 => C8
+    case 8 => C9
+    case 9 => C10
+    case 10 => C11
+    case 11 => C12
+    case 12 => C13
+    case 13 => C14
+    case 14 => C15
+    case 15 => C16
+    case 16 => C17
+    case 17 => C18
+    case 18 => C19
+    case 19 => C20
+    case 20 => C21
+    case 21 => C22
+    case 22 => C23
+    case 23 => C24
+    case 24 => C25
+    case 25 => C26
+    case 26 => C27
+    case 27 => C28
+    case 28 => C29
+    case 29 => C30
+    case 30 => C31
+    case 31 => C32
+    case 32 => C33
+    case 33 => C34
+    case 34 => C35
+    case 35 => C36
+    case 36 => C37
+    case 37 => C38
+    case 38 => C39
+    case 39 => C40
+    case 40 => C41
+    case 41 => C42
+    case 42 => C43
+    case 43 => C44
+    case 44 => C45
+    case 45 => C46
+    case 46 => C47
+    case 47 => C48
+    case 48 => C49
+    case 49 => C50
+    case 50 => C51
+    case 51 => C52
+    case 52 => C53
+    case 53 => C54
+    case 54 => C55
+    case 55 => C56
+    case 56 => C57
+    case 57 => C58
+    case 58 => C59
+    case 59 => C60
+    case 60 => C61
+    case 61 => C62
+    case 62 => C63
+    case 63 => C64
+    case 64 => C65
+    case 65 => C66
+    case 66 => C67
+    case 67 => C68
+    case 68 => C69
+    case 69 => C70
+    case 70 => C71
+    case 71 => C72
+    case 72 => C73
+    case 73 => C74
+    case 74 => C75
+    case 75 => C76
+    case 76 => C77
+    case 77 => C78
+    case 78 => C79
+    case 79 => C80
+    case 80 => C81
+    case 81 => C82
+    case 82 => C83
+    case 83 => C84
+    case 84 => C85
+    case 85 => C86
+    case 86 => C87
+    case 87 => C88
+    case 88 => C89
+    case 89 => C90
+    case 90 => C91
+    case 91 => C92
+    case 92 => C93
+    case 93 => C94
+    case 94 => C95
+    case 95 => C96
+    case 96 => C97
+    case 97 => C98
+    case 98 => C99
+    case 99 => C100
+    case 100 => C101
+    case 101 => C102
+    case 102 => C103
+    case 103 => C104
+    case 104 => C105
+    case 105 => C106
+    case 106 => C107
+    case 107 => C108
+    case 108 => C109
+    case 109 => C110
+    case 110 => C111
+    case 111 => C112
+    case 112 => C113
+    case 113 => C114
+    case 114 => C115
+    case 115 => C116
+    case 116 => C117
+    case 117 => C118
+    case 118 => C119
+    case 119 => C120
+    case 120 => C121
+    case 121 => C122
+    case 122 => C123
+    case 123 => C124
+    case 124 => C125
+    case 125 => C126
+    case 126 => C127
+    case 127 => C128
+    case 128 => C129
+    case 129 => C130
+    case 130 => C131
+    case 131 => C132
+    case 132 => C133
+    case 133 => C134
+    case 134 => C135
+    case 135 => C136
+    case 136 => C137
+    case 137 => C138
+    case 138 => C139
+    case _ => throw new IndexOutOfBoundsException(n.toString)
+  }
+
+  override def toString: String = {
+    val sb = new StringBuffer
+    for (i <- 0 until productArity)
+      sb.append(productElement(i)).append(',')
+    sb.toString
+  }
+
+  def isWrongRow(): Boolean = (0 until productArity)
+    .map(idx => productElement(idx))
+    .forall(e => e == None)
+}
+
+/** A dummy csv parser for orders dataset. */
+object POIParse extends Serializable {
+  def apply(row: Array[String]): Order = {
+
+    import water.support.ParseSupport._
+
+    new Order(str(row(0)), // order
+      str(row(1)), // driver
+      str(row(2)), // passenger
+      str(row(3)), // start
+      str(row(4)), // dest
+      float(row(5)), // price
+      str(row(6)) // time
+
+    )
+  }
+}
+
